0e558783a9f20dd867c3217bb6be891391fac91e
Hypnoes <hypnoes@qq.com>
2018-10-22 20:16:38 +0800

    typo

---
+++ chapterC/placenew1.cpp
@@ -0,0 +1,52 @@
+// placenew1.cpp -- new, placement new, no delete
+#include <iostream>
+#include <string>
+#include <new>
+
+using namespace std;
+const int BUF = 512;
+
+class JustTesting
+{
+  private:
+    string words;
+    int number;
+
+  public:
+    JustTesting(const string & s = "Just Testing", int n = 0)
+    { words = s; number = n; cout << words << " constructed\n"; }
+    ~JustTesting() { cout << words << " destroyed\n"; }
+
+    void show() const { cout << words << ", " << number << endl; }
+};
+
+int main()
+{
+    char * buffer = new char[BUF];
+    JustTesting *pc1, *pc2;
+    pc1 = new (buffer) JustTesting;
+    pc2 = new JustTesting("Heap1", 20);
+
+    cout << "Memory bolck address:\n" << "buffer: "
+         << (void *) buffer << "\theap: " << pc2 << endl;
+    cout << "Memory contents:\n";
+    cout << pc1 << ": ";
+    pc1->show();
+    pc2->show();
+
+    JustTesting *pc3, *pc4;
+    pc3 = new (buffer) JustTesting("Bad Idea", 6);
+    pc4 = new JustTesting("Heap2", 10);
+
+    cout << "Memory conttents:\n";
+    cout << pc3 << ": ";
+    pc3->show();
+    cout << pc4 << ": ";
+    pc4->show();
+
+    delete pc2;
+    delete pc4;
+    delete [] buffer;
+    cout << "Done\n";
+    return 0;
+}
---
+++ chapterC/queue/main.cpp
@@ -0,0 +1,92 @@
+// main.cpp -- using the Queue interface
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "queue.h"
+const int MIN_PER_HR = 60;
+
+bool new_customer(double x);
+
+int main()
+{
+    using std::cin;
+    using std::cout;
+    using std::endl;
+    using std::ios_base;
+    std::srand(std::time(0));
+
+    cout << "Case Study: Bank of Heather Automatic Teller\n";
+    cout << "Enter maximum size of queue: ";
+    int qs;
+    cin >> qs;
+    Queue line(qs);
+
+    cout << "Enter the number of simulation hours: ";
+    int hours;
+    cin >> hours;
+
+    long cyclelimit = MIN_PER_HR * hours;
+
+    cout << "Enter the average number of customers per hour: ";
+    double perhour;
+    cin >> perhour;
+    double min_per_cust;
+    min_per_cust = MIN_PER_HR;
+
+    Item temp;
+    long turn_aways = 0;
+    long customers = 0;
+    long served = 0;
+    long sum_line = 0;
+    int wait_time = 0;
+    long line_wait = 0;
+
+    for (int cycle = 0; cycle < cyclelimit; cycle++)
+    {
+        if (new_customer(min_per_cust))
+        {
+            if (line.is_full())
+                turn_aways++;
+            else
+            {
+                customers++;
+                temp.set(cycle);
+                line.enqueue(temp);
+            }
+        }
+        if (wait_time <= 0 && !line.is_empty())
+        {
+            line.dequeue(temp);
+            wait_time = temp.ptime();
+            line_wait += cycle - temp.when();
+            served++;
+        }
+        if (wait_time > 0)
+            wait_time--;
+        sum_line += line.count();
+    }
+
+    if (customers > 0)
+    {
+        cout << "customers accepted: " << customers << endl;
+        cout << "  customers served: " << served << endl;
+        cout << "         turnaways: " << turn_aways << endl;
+        cout << "average queue size: ";
+        cout.precision(2);
+        cout.setf(ios_base::fixed, ios_base::floatfield);
+        cout << (double) sum_line / cyclelimit << endl;
+        cout << " average wait time: "
+             << (double) line_wait / served << " minutes\n";
+    }
+    else
+        cout << "No customers!\n";
+    cout << "Done!\n";
+
+    return 0;
+}
+
+bool new_customer(double x)
+{
+    return (std::rand() * x / RAND_MAX < 1);
+}
